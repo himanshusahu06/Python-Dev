e4c556cd453325e894e6d897963fa8cfe6e6e83e
Himanshu Sahu <himanshusahu24@gmail.com>
2018-02-26 18:07:36 +0530

    smo extra things

--- graphQL/hackernews/links/schema.py
+++ graphQL/hackernews/links/schema.py
@@ -1,7 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from links.models import Link
+from links.models import Link, Vote
+from users.schema import get_user, UserType
 
 ######################## Query ########################
 
@@ -9,13 +10,20 @@ class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
 ## define Types
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
+    votes = graphene.List(VoteType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 '''
 query  {
@@ -37,6 +45,7 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     #2: Defines the data you can send to the server.
     class Arguments:
@@ -46,19 +55,46 @@ class CreateLink(graphene.Mutation):
     #3: The mutation method: it creates a link on the database using the data sent by the user,through the url and description parameters.
     # After, the server returns the CreateLink class with the data just created.
     def mutate(self, info, url, description):
-        link = Link(url = url, description = description)
+        user = get_user(info) or None
+        link = Link(
+            url = url,
+            description = description,
+            posted_by=user
+        )
         ## save the date
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
-            description=link.description
+            description=link.description,
+            posted_by=link.posted_by
         )
 
-#4: Creates a mutation class with a field to be resolved, which points to our mutation defined before.
-class Mutation(graphene.ObjectType):
-    create_link = CreateLink.Field()
+
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = get_user(info) or None
+        if not user:
+            raise Exception("You must be logged in to vote!!!")
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception("Invalid Link!!")
+        
+        Vote.objects.create(
+            user=user,
+            link=link
+        )
+
+        return CreateVote(user=user, link=link)
+
 
 '''
 mutation {
@@ -70,3 +106,7 @@ mutation {
 }
 '''
 
+#4: Creates a mutation class with a field to be resolved, which points to our mutation defined before.
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
